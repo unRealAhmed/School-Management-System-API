7b9da6512bba162e21c72196b262c44c8ebd1d88
Ahmed Sayed <therealahme@gmail.com>
2023-11-19 10:14:33 +0200

    add password authentication methods

--- models/Staff/Admin.js
+++ models/Staff/Admin.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const crypto = require('crypto');
+const bcrypt = require('bcryptjs');
 const { default: validator } = require("validator");
 
 const adminSchema = new mongoose.Schema(
@@ -88,6 +90,71 @@ const adminSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
+
+// COMPARING PASSWORDS
+adminSchema.methods.passwordMatching = async function (enteredPassword, userPassword) {
+  return await bcrypt.compare(enteredPassword, userPassword);
+};
+
+// PASSWORD CHANGE CHECK
+adminSchema.methods.changedPasswordAfter = function (tokenIssuedAt) {
+  if (this.passwordChangedAt) {
+    // Convert passwordChangedAt timestamp to seconds
+    const changedTimestamp = this.passwordChangedAt.getTime() / 1000;
+    return tokenIssuedAt < changedTimestamp;
+  }
+  return false;
+};
+
+//PASSWORD CHANGE CHECKER
+adminSchema.methods.changedPasswordAfter = function (tokenIssuedAt) {
+  if (this.passwordChangedAt) {
+    // Convert passwordChangedAt timestamp to seconds
+    const changedTimestamp = this.passwordChangedAt.getTime() / 1000;
+    return tokenIssuedAt < changedTimestamp;
+  }
+  return false;
+};
+
+// HASH PASSWORD before saving the user
+adminSchema.pre('save', async function (next) {
+  // Check if the password field has been modified
+  if (!this.isModified('password')) return next();
+
+  try {
+    // Generate a salt with a cost factor of 12
+    const salt = await bcrypt.genSalt(12);
+    this.password = await bcrypt.hash(this.password, salt);
+
+    // Set passwordConfirm to undefined as it's no longer needed
+    this.passwordConfirm = undefined;
+
+    // Update passwordChangedAt if it's not a new user
+    if (!this.isNew) this.passwordChangedAt = Date.now() - 1000;
+
+    next();
+  } catch (error) {
+    return next(error);
+  }
+});
+
+//PASSWORD RESET TOKEN GENERATOR
+adminSchema.methods.createPasswordResetToken = function () {
+  // Generate a random reset token
+  const resetToken = crypto.randomBytes(32).toString("hex");
+
+  // Hash the token and set it on the user
+  this.passwordResetToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+
+  // Set an expiration time for the token (10 minutes)
+  this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+
+  // Return the unhashed token for use in the email
+  return resetToken;
+};
 
 // Model
 const Admin = mongoose.model("Admin", adminSchema);
