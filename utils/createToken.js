9ef31928bd01504673223d358a8f1f48eb893099
Ahmed Sayed <therealahme@gmail.com>
2023-11-19 10:13:54 +0200

    create the token generator

---
+++ utils/createToken.js
@@ -0,0 +1,24 @@
+const JWT = require("jsonwebtoken");
+
+// Create a JWT token with user's id and set it as a cookie
+const createToken = (res, id) => {
+  // Create a JWT token
+  const token = JWT.sign({ id }, process.env.JWT_SECRET_KEY, {
+    expiresIn: process.env.JWT_EXPIRES,
+  });
+
+  // Configure the options for the cookie
+  const cookieOptions = {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    signed: true,
+    sameSite: "strict",
+    maxAge: 1000 * 60 * 60 * 24, // Maximum age of the cookie (one day)
+  };
+
+  res.cookie("token", token, cookieOptions);
+
+  return token;
+};
+
+module.exports = createToken;
